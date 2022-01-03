8cf9a00be747da9b7c1f55657956c1b39c76f98c
saidulislam <saidul.islam@gmail.com>
2022-01-02 23:37:45 -0500

    password reset via email

---
+++ 20-password-reset-via-email/project/models.py
@@ -0,0 +1,107 @@
+from project import database, bcrypt
+from flask import current_app
+from datetime import datetime
+
+class Stock(database.Model):
+    """
+    Class that represents a purchased stock in a portfolio.
+
+    The following attributes of a stock are stored in this table:
+        stock symbol (type: string)
+        number of shares (type: integer)
+        purchase price (type: integer)
+
+    Note: Due to a limitation in the data types supported by SQLite, the
+          purchase price is stored as an integer:
+              $24.10 -> 2410
+              $100.00 -> 10000
+              $87.65 -> 8765
+    """
+
+    __tablename__ = 'stocks'
+
+    id = database.Column(database.Integer, primary_key=True)
+    stock_symbol = database.Column(database.String, nullable=False)
+    number_of_shares = database.Column(database.Integer, nullable=False)
+    purchase_price = database.Column(database.Integer, nullable=False)
+
+    def __init__(self, stock_symbol: str, number_of_shares: str, purchase_price: str):
+        self.stock_symbol = stock_symbol
+        self.number_of_shares = int(number_of_shares)
+        self.purchase_price = int(float(purchase_price) * 100)
+
+    def __repr__(self):
+        return f'{self.stock_symbol} - {self.number_of_shares} shares purchased at ${self.purchase_price / 100}'
+
+
+class User(database.Model):
+    """
+    Class that represents a user of the application
+
+    The following attributes of a user are stored in this table:
+        * email - email address of the user
+        * hashed password - hashed password (using Flask-Bcrypt)
+        * registered_on - date & time that the user registered
+        * email_confirmation_sent_on - date & time that the confirmation email was sent
+        * email_confirmed - flag indicating if the user's email address has been confirmed
+        * email_confirmed_on - date & time that the user's email address was confirmed
+
+    REMEMBER: Never store the plaintext password in a database!
+    """
+    __tablename__ = 'users'
+
+    id = database.Column(database.Integer, primary_key=True)
+    email = database.Column(database.String, unique=True)
+    password_hashed = database.Column(database.String(60))
+    registered_on = database.Column(database.DateTime)
+    email_confirmation_sent_on = database.Column(database.DateTime)
+    email_confirmed = database.Column(database.Boolean, default=False)
+    email_confirmed_on = database.Column(database.DateTime)
+
+    def __init__(self, email: str, password_plaintext: str):
+        """Create a new User object
+
+        This constructor assumes that an email is sent to the new user to confirm
+        their email address at the same time that the user is registered.
+        """
+        self.email = email
+        self.password_hashed = self._generate_password_hash(password_plaintext)
+        self.registered_on = datetime.now()
+        self.email_confirmation_sent_on = datetime.now()
+        self.email_confirmed = False
+        self.email_confirmed_on = None
+
+    def is_password_correct(self, password_plaintext: str):
+        return bcrypt.check_password_hash(self.password_hashed, password_plaintext)
+
+    def __repr__(self):
+        return f'<User: {self.email}>'
+
+    @property
+    def is_authenticated(self):
+        """Return True if the user has been successfully registered."""
+        return True
+
+    @property
+    def is_active(self):
+        """Always True, as all users are active."""
+        return True
+
+    @property
+    def is_anonymous(self):
+        """Always False, as anonymous users aren't supported."""
+        return False
+
+    def get_id(self):
+        """Return the user ID as a unicode string (`str`)."""
+        return str(self.id)
+
+    def set_password(self, password_plaintext: str):
+        self.password_hashed = self._generate_password_hash(password_plaintext)
+
+    @staticmethod
+    def _generate_password_hash(password_plaintext):
+        return bcrypt.generate_password_hash(
+            password_plaintext,
+            current_app.config.get('BCRYPT_LOG_ROUNDS')
+        ).decode('utf-8')
